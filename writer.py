289d16f87ba5a8b9d421f8b31e04082a1e654f7f
Dave Barbalato <dmb@captivationsoftware.com>
2021-10-05 18:24:49 -0700

    better args

--- writer.py
+++ writer.py
@@ -9,15 +9,13 @@ data_bytes_1 = b'\01' * int(sys.argv[2])
 data_bytes_2 = b'\11' * int(sys.argv[2])
 
 try:
-    c = 0
+    data = data_bytes_1
     while True:
-        data =  f'{c}'.encode('utf8')
+        if data == data_bytes_1:
+            data = data_bytes_2
+        else:
+            data = data_bytes_1
         lib.momentum_send(context, sys.argv[1].encode('utf8'), data, len(data))
-        c += 1
-        # buffer = lib.momentum_acquire_buffer(context, b'foo', int(1e6))
-        # local_buffer.from_buffer(buffer)
-        # local_buffer[0] = c
-        # lib.momentum_send(context, b'foo', buffer, len(message))
 
 except KeyboardInterrupt:
     pass
