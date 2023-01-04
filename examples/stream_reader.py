ec3e8ff5f45dbdfd5bbe32af6415bd68ab267e67
Brad Sz <BradSz@outlook.com>
2023-01-04 14:34:00 -0500

    Updated example for non-blocking API

--- examples/stream_reader.py
+++ examples/stream_reader.py
@@ -7,8 +7,6 @@ import signal
 STREAM = "mx://streamer"
 THRESHOLD = 10000
 
-cancel = threading.Event()
-signal.signal(signal.SIGINT, (lambda _sig, _frm: cancel.set()))
 
 now = time.time()
 bytes_received = 0
@@ -16,22 +14,24 @@ messages_received = 0
 last_iteration = 0
 skip_count = 0
 
-stream = mx.Consumer(STREAM, cancel, polling_interval=0.0)
-for buffer in iter(stream.receive, None):
-    messages_received += 1
-    bytes_received += len(bytearray(buffer))
+stream = mx.Consumer(STREAM)
 
-    step = buffer.iteration - last_iteration
-    if last_iteration > 0 and step > 1:
-        skip_count = skip_count + step
+while stream.is_alive:
+    buffer = stream.receive(blocking=False)
 
-    last_iteration = buffer.iteration
+    if buffer:
+        messages_received += 1
+        bytes_received += len(bytearray(buffer))
 
-    if messages_received % THRESHOLD == 0:
-        elapsed = time.time() - now
-        print("Received {:.2f} msgs/sec".format(messages_received / elapsed))
-        print("Received {:.2f} GB/sec".format(bytes_received / elapsed / 1.0e9))
-        print(f"Missed: {skip_count}")
-        skip_count = 0
+        step = buffer.iteration - last_iteration
+        if last_iteration > 0 and step > 1:
+            skip_count = skip_count + step
 
-    del buffer
+        last_iteration = buffer.iteration
+
+        if messages_received % THRESHOLD == 0:
+            elapsed = time.time() - now
+            print("Received {:.2f} msgs/sec".format(messages_received / elapsed))
+            print("Received {:.2f} GB/sec".format(bytes_received / elapsed / 1.0e9))
+            print(f"Missed: {skip_count}")
+            skip_count = 0
--- examples/stream_writer.py
+++ examples/stream_writer.py
@@ -1,43 +1,39 @@
 import time
 
 import momentumx as mx
-import threading
-import signal
 
 
 STREAM = "mx://streamer"
 ts_old = round(time.time())
-
-cancel = threading.Event()
-signal.signal(signal.SIGINT, (lambda _sig, _frm: cancel.set()))
 
 now = time.time()
 messages_sent = 0
 bytes_sent = 0
 data_length = int(100e6)
 
-stream = mx.Producer(STREAM, data_length, 30, True, cancel, polling_interval=0.0)
+stream = mx.Producer(STREAM, data_length, 30, True)
 
 while stream.subscriber_count == 0:
     print("waiting for subscriber(s)")
-    if cancel.wait(0.5):
-        break
+    time.sleep(0.5)
 
+while stream.is_alive:
+    buffer = stream.next_to_send(blocking=False)
 
-for buffer in iter(stream.next_to_send, None):
-    buffer.send(data_length)
+    if buffer:
+        buffer.send(data_length)
 
-    messages_sent += 1
-    bytes_sent += data_length
+        messages_sent += 1
+        bytes_sent += data_length
 
-    ts_new = round(time.time())
-    if ts_new != ts_old:
-        ts_old = ts_new
-        elapsed = time.time() - now
-        print("Sent {:.2f} msgs/sec".format(messages_sent / elapsed))
-        print("Sent {:.2f} GB/sec".format(bytes_sent / elapsed / 1.0e9))
+        ts_new = round(time.time())
+        if ts_new != ts_old:
+            ts_old = ts_new
+            elapsed = time.time() - now
+            print("Sent {:.2f} msgs/sec".format(messages_sent / elapsed))
+            print("Sent {:.2f} GB/sec".format(bytes_sent / elapsed / 1.0e9))
 
-        messages_sent = 0
-        bytes_sent = 0
+            messages_sent = 0
+            bytes_sent = 0
 
-        now = time.time()
+            now = time.time()
