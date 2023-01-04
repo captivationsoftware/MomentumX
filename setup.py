3bf5d073394aa1a4a9099b1a7ebd7b0090e0345a
Dave Barbalato <dmb@captivationsoftware.com>
2023-01-04 10:34:37 -0800

    Fixup

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import time
-
 from setuptools import find_packages
 from skbuild import setup
 
@@ -10,14 +8,9 @@ setup(
     version=__version__,
     description="Zero-copy shared memory IPC library for building complex streaming data pipelines capable of processing large datasets",
     author="Captivation Software, LLC",
-    # packages=find_packages(),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     cmake_install_dir="src/momentumx",
-    # include_package_data=True,
     extras_require={"test": ["pytest"]},
     python_requires=">=3.7",
-    # cmake_args=[
-    #     "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
-    # ]
 )
